4adba65f16a2ad16a91e84502be30528538f12e7
satyamnaithani <satyamnaithani99@gmail.com>
2019-03-20 19:23:00 +0530

    Updates

---
+++ precache-manifest.1699c9aefc02320b78b4ee61cb547052.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "64086e7b7c0e65907c4f",
+    "url": "/Jan-Elaaj-Website-REACT/static/css/main.7451fa59.chunk.css"
+  },
+  {
+    "revision": "64086e7b7c0e65907c4f",
+    "url": "/Jan-Elaaj-Website-REACT/static/js/main.b1999918.chunk.js"
+  },
+  {
+    "revision": "8f357b2b65b98200a713",
+    "url": "/Jan-Elaaj-Website-REACT/static/js/runtime~main.a957ae22.js"
+  },
+  {
+    "revision": "5176793bd55f3ee58f74",
+    "url": "/Jan-Elaaj-Website-REACT/static/css/2.9d350601.chunk.css"
+  },
+  {
+    "revision": "5176793bd55f3ee58f74",
+    "url": "/Jan-Elaaj-Website-REACT/static/js/2.2ff815c9.chunk.js"
+  },
+  {
+    "revision": "87df18c25980991ac59d0a79fbf0ccf5",
+    "url": "/Jan-Elaaj-Website-REACT/static/media/doc.87df18c2.jpg"
+  },
+  {
+    "revision": "6113e58304d37891feb20a2f67e47a48",
+    "url": "/Jan-Elaaj-Website-REACT/index.html"
+  }
+];
